2a21762ca667e31c0be1dd005a49f747c5681e8c
jmcooper <jim@cooperplanet.com>
2017-11-08 20:35:37 -0700

    Fix image urls

--- misc/event-data.js
+++ misc/event-data.js
@@ -5,7 +5,7 @@ events = [
       date: '9/26/2036',
       time: '10:00 am',
       price: 599.99,
-      imageUrl: '/app/assets/images/angularconnect-shield.png',
+      imageUrl: '/assets/images/angularconnect-shield.png',
       location: {
         address: '1057 DT',
         city: 'London',
@@ -83,7 +83,7 @@ events = [
       date: '4/15/2037',
       time: '9:00 am',
       price: 950.00,
-      imageUrl: '/app/assets/images/ng-nl.png',
+      imageUrl: '/assets/images/ng-nl.png',
       location: {
         address: 'The NG-NL Convention Center & Scuba Shop',
         city: 'Amsterdam',
@@ -143,7 +143,7 @@ events = [
       date: '5/4/2037',
       time: '9:00 am',
       price: 759.00,
-      imageUrl: '/app/assets/images/ng-conf.png',
+      imageUrl: '/assets/images/ng-conf.png',
       location: {
         address: 'The Palatial America Hotel',
         city: 'Salt Lake City',
@@ -225,7 +225,7 @@ events = [
       date: '6/10/2037',
       time: '8:00 am',
       price: 800.00,
-      imageUrl: '/app/assets/images/basic-shield.png',
+      imageUrl: '/assets/images/basic-shield.png',
       location: {
         address: 'The UN Angular Center',
         city: 'New York',
@@ -274,7 +274,7 @@ events = [
       date: '2/10/2037',
       time: '9:00 am',
       price: 400.00,
-      imageUrl: '/app/assets/images/ng-vegas.png',
+      imageUrl: '/assets/images/ng-vegas.png',
       location: {
         address: 'The Excalibur',
         city: 'Las Vegas',
